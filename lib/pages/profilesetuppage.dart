fb4b2004120b1a481c9c85aff83a9410bec25d82
Rahul Gupta <rahulguptaid1@gmail.com>
2025-10-16 17:04:10 +0530

    Complete basic frontend setup, ready to connect backend

--- lib/pages/profilesetuppage.dart
+++ lib/pages/profilesetuppage.dart
@@ -1,7 +1,7 @@
 import 'package:app/pages/genereselectionpage.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
 
 class ProfileSetupPage extends StatefulWidget {
   final String email;
